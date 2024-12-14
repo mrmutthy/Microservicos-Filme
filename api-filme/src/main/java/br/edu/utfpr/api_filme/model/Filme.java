6b96c31d381fcad8c3eb5e977d446623c95b06a8
Lucas da Silva Marcos <640383+lucasmarcos@users.noreply.github.com>
2024-12-14 15:02:57 -0300

    use lombok on Filme model

--- api-filme/src/main/java/br/edu/utfpr/api_filme/model/Filme.java
+++ api-filme/src/main/java/br/edu/utfpr/api_filme/model/Filme.java
@@ -2,78 +2,27 @@ package br.edu.utfpr.api_filme.model;
 
 import jakarta.persistence.*;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Filme {
+
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "filme_seq")
     @SequenceGenerator(name = "filme_seq", sequenceName = "filme_seq", allocationSize = 1)
-
     private Long id;
+
     private String titulo;
     private String autor;
     private String genero;
     private String dataLancamento;
     private String sinopse;
-
-    public Filme(Long id, String titulo, String autor, String genero, String dataLancamento, String sinopse) {
-        this.id = id;
-        this.titulo = titulo;
-        this.autor = autor;
-        this.genero = genero;
-        this.dataLancamento = dataLancamento;
-        this.sinopse = sinopse;
-    }
-
-    public Filme() {
-
-    }
-
-    //Getters
-
-
-
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public String getAutor() {
-        return autor;
-    }
-
-    public String getGenero() {
-        return genero;
-    }
-
-    public String getDataLancamento() {
-        return dataLancamento;
-    }
-
-    public String getSinopse() {
-        return sinopse;
-    }
-
-    // Setters
-
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public void setAutor(String autor) {
-        this.autor = autor;
-    }
-
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-
-    public void setDataLancamento(String dataLancamento) {
-        this.dataLancamento = dataLancamento;
-    }
-
-    public void setSinopse(String sinopse) {
-        this.sinopse = sinopse;
-    }
 
 }
