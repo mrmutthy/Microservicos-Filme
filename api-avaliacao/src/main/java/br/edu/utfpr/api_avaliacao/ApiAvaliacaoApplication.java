56cabdba6fd9e6594a3d828c1c376c81a6dc15b5
Lucas da Silva Marcos <640383+lucasmarcos@users.noreply.github.com>
2024-12-14 16:24:09 -0300

    EnableFeignClients

--- api-avaliacao/src/main/java/br/edu/utfpr/api_avaliacao/ApiAvaliacaoApplication.java
+++ api-avaliacao/src/main/java/br/edu/utfpr/api_avaliacao/ApiAvaliacaoApplication.java
@@ -2,8 +2,10 @@ package br.edu.utfpr.api_avaliacao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ApiAvaliacaoApplication {
 
 	public static void main(String[] args) {
