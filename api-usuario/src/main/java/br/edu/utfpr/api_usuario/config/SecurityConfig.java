a385dc4c631f55647ed6d092bb72d6beb50a675d
Lucas da Silva Marcos <640383+lucasmarcos@users.noreply.github.com>
2025-02-08 12:27:19 -0300

    add SecurityConfig

---
+++ api-usuario/src/main/java/br/edu/utfpr/api_usuario/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package br.edu.utfpr.api_usuario.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(auth -> auth
+			.requestMatchers("/h2-console/**").permitAll()
+			.requestMatchers(HttpMethod.POST, "/usuarios").permitAll()
+			.anyRequest().authenticated()
+		);
+
+		http.csrf(AbstractHttpConfigurer::disable);
+		http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+
+		return http.build();
+	}
+
+}
