f973745e03a6e01eeb7f31db3d5f187223becc4b
Lucas da Silva Marcos <640383+lucasmarcos@users.noreply.github.com>
2025-02-08 13:05:55 -0300

    add findByEmail

--- api-usuario/src/main/java/br/edu/utfpr/api_usuario/repositories/UsuarioRepository.java
+++ api-usuario/src/main/java/br/edu/utfpr/api_usuario/repositories/UsuarioRepository.java
@@ -3,4 +3,6 @@ package br.edu.utfpr.api_usuario.repositories;
 import br.edu.utfpr.api_usuario.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {}
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	Usuario findByEmail(String email);
+}
