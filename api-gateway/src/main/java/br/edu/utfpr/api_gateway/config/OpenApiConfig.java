7332bde35e4bde2390cc19536e93e562f7b241e5
Lucas da Silva Marcos <640383+lucasmarcos@users.noreply.github.com>
2025-02-09 15:43:17 -0300

    add swagger

---
+++ api-gateway/src/main/java/br/edu/utfpr/api_gateway/config/OpenApiConfig.java
@@ -0,0 +1,9 @@
+package br.edu.utfpr.api_gateway.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition
+public class OpenApiConfig {
+}
